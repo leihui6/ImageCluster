f1251978d189df48e417e78ed004b9cc2e7eaa47
gltina <ptsph@foxmail.com>
2020-02-03 15:07:27 +0100

    remove the background basically

--- Source.cpp
+++ Source.cpp
@@ -10,12 +10,17 @@ using namespace cv;
 
 using namespace std;
 
+void background_removal(cv::Mat & _img, cv::Mat & _res_img,int _r, int _g, int _b, float _threshold);
+
 int main()
 {
 #ifdef _ONLINE_DEBUG_
 
 	cv::VideoCapture cap;
-	cap.open("sample/special_test2.mp4");
+
+	//cap.open(0);
+
+	cap.open("sample/special_test.mp4");
 
 	if (!cap.isOpened())
 	{
@@ -26,12 +31,16 @@ int main()
 	
 	cap >> frame;
 
-	Mat img_bin(cv::Size(frame.cols, frame.rows), CV_8UC1);
+	VideoWriter demo_video("./demo.mp4", VideoWriter::fourcc('M', 'J', 'P', 'G'), 30, frame.size(), true);
+
+	Mat img_bin, img_removed_bg;
 
 	ImageCluster image_cluster;
 
 	while (cap.isOpened())
 	{
+		clock_t begin_time = clock();
+
 		cap >> frame;
 
 		if (frame.empty())
@@ -39,21 +48,23 @@ int main()
 			return 0;
 		}
 
-		cv::cvtColor(frame, img_bin, COLOR_RGB2GRAY);
+		background_removal(frame, img_removed_bg, 255, 255, 255, 250.0);
+
+		cv::cvtColor(img_removed_bg, img_bin, COLOR_RGB2GRAY);
 
 		cv::threshold(img_bin, img_bin, 125, 255, cv::THRESH_OTSU);
 
 		cv::bitwise_not(img_bin, img_bin);
 
-		clock_t begin_time = clock();
-
 		image_cluster.load_image(img_bin.data, img_bin.cols, img_bin.rows);
 
 		image_cluster.init_kernel_size(8, 12);
 
-		image_cluster.cluster(0);
+		image_cluster.cluster(20);
 
-		std::cout << "execution time:" << (double)(clock() - begin_time) << "ms" << std::endl;
+		clock_t end_time = clock();
+
+		//std::cout << "execution time:" << (double)(end_time - begin_time) << "ms" << std::endl;
 
 		cv::Mat cluster_image;
 
@@ -90,15 +101,15 @@ int main()
 			total_clusters[i].get_center_point(cp);
 
 			// draw the cluster
-			for (int j = 0; j < cluster_pixels.size(); ++j)
-			{
-				// blue green red
-				cv::Vec3b &color = cluster_image.at<cv::Vec3b>(cluster_pixels[j]);
+			//for (int j = 0; j < cluster_pixels.size(); ++j)
+			//{
+			//	// blue green red
+			//	cv::Vec3b &color = cluster_image.at<cv::Vec3b>(cluster_pixels[j]);
 
-				color[0] = 255;
-				color[1] = 255;
-				color[2] = 255;
-			}
+			//	color[0] = 255;
+			//	color[1] = 255;
+			//	color[2] = 255;
+			//}
 
 			for (int j = 0; j < 4; ++j)
 			{
@@ -117,6 +128,9 @@ int main()
 
 			// draw the serial number of cluster
 			cv::putText(cluster_image, std::to_string(i), cp, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar::all(0));
+
+			// put the fps on screen
+			cv::putText(cluster_image, "FPS:"+std::to_string((int(1000/(end_time-begin_time)))), cv::Point2i(0,20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar::all(0));
 		}
 
 		cv::imshow("original image", frame);
@@ -125,15 +139,23 @@ int main()
 
 		cv::imshow("cluster_image", cluster_image);
 
+		demo_video << cluster_image;
+
 		cv::waitKey(33);
 
 		image_cluster.clear();
 	}
+
+	cap.release();
+
 #endif // _ONLINE_DEBUG_
 
 #ifdef _LOCAL_DEBUG_
 
-	Mat img = imread("sample/special_test.jpg");
+	cv::Mat img = cv::imread("sample/photo2.jpg");
+
+	//Mat background = imread("sample/background.jpg");
+	//img = img - background;
 
 	if (!img.data)
 	{
@@ -141,17 +163,28 @@ int main()
 		return -1;
 	}
 
-	cv::resize(img, img,cv::Size(480,640));
+	Mat img_removed_bg;
 
-	Mat img_bin(cv::Size(img.rows, img.cols), CV_8UC1);
+	Mat img_bin(cv::Size(img.cols, img.rows), CV_8UC1);
 
-	cout << "width=" << img.cols << " height=" << img.rows << endl;
+	background_removal(img, img_removed_bg, 255, 255, 255, 200.0);
+
+	cv::resize(img, img, cv::Size(480, 640));
+	cv::resize(img_removed_bg, img_removed_bg, cv::Size(480, 640));
+
+	std::cout << "width=" << img_removed_bg.cols << " height=" << img_removed_bg.rows << std::endl;
 	
-	cv::cvtColor(img, img_bin, COLOR_RGB2GRAY);
+	cv::cvtColor(img_removed_bg, img_bin, COLOR_RGB2GRAY);
+
+	cv::imshow("original image", img);
+
+	cv::imshow("gray image", img_bin);
 
 	cv::threshold(img_bin, img_bin, 125, 255, cv::THRESH_OTSU);
 
-	bitwise_not(img_bin, img_bin);
+	cv::bitwise_not(img_bin, img_bin);
+
+	cv::imshow("threshold image", img_bin);
 
 	ImageCluster image_cluster;
 	
@@ -159,7 +192,7 @@ int main()
 
 	image_cluster.load_image(img_bin.data,img_bin.cols,img_bin.rows);
 
-	image_cluster.init_kernel_size(8, 12);
+	image_cluster.init_kernel_size(8, 16);
 
 	image_cluster.cluster(10);
 
@@ -199,23 +232,23 @@ int main()
 		total_clusters[i].get_middle_points_of_lines(middle_points_of_lines);
 
 		// draw the cluster
-		for (int j = 0; j < cluster_pixels.size(); ++j)
-		{
-			// blue green red
-			cv::Vec3b &color =  cluster_image.at<cv::Vec3b>(cluster_pixels[j]);
-			
-			color[0] = 255;
-			color[1] = 255;
-			color[2] = 255;
-		}
+		//for (int j = 0; j < cluster_pixels.size(); ++j)
+		//{
+		//	// blue green red
+		//	cv::Vec3b &color = cluster_image.at<cv::Vec3b>(cluster_pixels[j]);
+
+		//	color[0] = 255;
+		//	color[1] = 255;
+		//	color[2] = 255;
+		//}
 
 		for (int j = 0; j < 4; ++j)
 		{
 			// draw the minimum box of cluster
-			line(cluster_image, min_box_rect[j], min_box_rect[(j + 1) % 4], cv::Scalar(0,255,0));
+			line(cluster_image, min_box_rect[j], min_box_rect[(j + 1) % 4], cv::Scalar(255, 0, 0));
 
 			// draw the middle points of lines(minimum box)
-			cv::circle(cluster_image, middle_points_of_lines[j], 5, cv::Scalar(0,0,255));
+			cv::circle(cluster_image, middle_points_of_lines[j], 5, cv::Scalar(0, 0, 255));
 		}
 
 		total_clusters[i].get_max_box(p1, p2);
@@ -223,7 +256,7 @@ int main()
 		total_clusters[i].get_center_point(cp);
 
 		// draw the maximum box of cluster
-		cv::rectangle(cluster_image, cv::Rect(p1, p2), cv::Scalar::all(255), 1);
+		cv::rectangle(cluster_image, cv::Rect(p1, p2), cv::Scalar(0, 255, 0), 1);
 
 		// draw the center point of cluster
 		cv::circle(cluster_image, cp, 3, cv::Scalar::all(0), -1);
@@ -232,10 +265,6 @@ int main()
 		cv::putText(cluster_image, std::to_string(i), cp, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar::all(0));
 	}
 
-	cv::imshow("original image", img);
-
-	cv::imshow("gray image", img_bin);
-
 	cv::imshow("cluster_image", cluster_image);
 
 	cv::waitKey();
@@ -243,5 +272,39 @@ int main()
 #endif // _LOCAL_DEBUG_
 
 
-	return 1;
+	return 0;
+}
+
+void background_removal(cv::Mat & _img, cv::Mat & _res_img, int _r, int _g, int _b, float _threshold)
+{
+	int r = 0, g = 0, b = 0;
+
+	_res_img.create(cv::Size(_img.cols, _img.rows), CV_8UC3);
+
+	for (int y = 0; y < _img.rows; ++y)
+	{
+		for (int x = 0; x < _img.cols; ++x)
+		{
+			cv::Vec3b & c = _img.at<cv::Vec3b>(y, x);
+
+			cv::Vec3b & c2 = _res_img.at<cv::Vec3b>(y, x);
+
+			b = c[0];
+			g = c[1];
+			r = c[2];
+			//cout << (sqrt((b - _b)*(b - _b) + (_g - g)*(_g - g) + (_r - r)*(_r - r))) <<endl;
+			if ((sqrt((b - _b)*(b - _b) + (_g - g)*(_g - g) + (_r - r)*(_r - r))) < _threshold)
+			{
+				c2[0] = 255;
+				c2[1] = 255;
+				c2[2] = 255;
+			}
+			else
+			{
+				c2[0] = 0;
+				c2[1] = 0;
+				c2[2] = 0;
+			}
+		}
+	}
 }
